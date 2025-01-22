3c0ecf52963bbdb479989a318d6e17f7ee7f2c91
pisha <pisha@hotmail.fr>
2025-01-22 11:47:59 +0100

    [INIT] - first init

---
+++ src/main/java/test/MCD_BDD/McdBddApplication.java
@@ -0,0 +1,13 @@
+package test.MCD_BDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McdBddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McdBddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/MCD_BDD/McdBddApplicationTests.java
@@ -0,0 +1,13 @@
+package test.MCD_BDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McdBddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
